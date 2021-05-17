08a8209d39e7f7e65d43ca0d7252c496a92e239c
venkeyy <60574581+venkeyy@users.noreply.github.com>
2021-05-17 10:34:51 +0530

    Update Day1-Data Types.py

--- Day1-Data Types.py
+++ Day1-Data Types.py
@@ -10,8 +10,9 @@ input_string=str(input())
 print(i+ input_int)
 
 # Print the sum of the double variables on a new line.
-print(round(d+input_double, 1))
-
+print(round(d+input_double, 1)) #The round keyword is used to round the value.
+                                 # Without using the round keyword the output was 8.01
+                                # So to print 8.0 round it to 1. 
 # Concatenate and print the String variables on a new line
 print(s+ input_string)
 
